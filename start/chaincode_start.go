519f6ccf4ddebe2e5fd6690daf7f80ff91cd979e
Chuck <indefire@gmail.com>
2017-01-31 14:39:59 -0500

    added

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -81,7 +81,7 @@ func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string)
 	var key, value string
 	var err error
 	fmt.Println("running write()")
-	fmt.Println("write is running checked IN")
+	fmt.Println("write is running checked IN 2")
 	if len(args) != 2 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the key and value to set")
 	}
